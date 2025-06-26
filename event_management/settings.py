e45ccaa29ebb21b17588c9cccec11039b3817ddc
mashukabashar <diyamoni422@gmail.com>
2025-06-26 17:41:13 +0600

    module-5-complete

--- event_management/settings.py
+++ event_management/settings.py
@@ -73,12 +73,25 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'event-management',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
+
 
 
 # Password validation
--- events/models.py
+++ events/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Participant(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    # events = models.ManyToManyField(Event, related_name='participants')
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Event(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="event"
+    )
+    # Category class niche likhle double quotation e Category likhte hobe Event class e....
+
+    guests=models.ManyToManyField(Participant, related_name="events")
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name} on {self.date}"
+
