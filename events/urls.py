b89726848287f06995c8ba7ce5556dc47213b4f5
mashukabashar <diyamoni422@gmail.com>
2025-06-26 11:46:07 +0600

    module 4.4

--- events/urls.py
+++ events/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from events.views import show_event, contact, show_specific_event
+from events.views import dashboard, test
 
 urlpatterns = [
-    path('show_event/',show_event),
-    path('contact/',contact),
-    path("show_event/<int:id>/",show_specific_event),
-    # path("show_event/<id>/",show_specific_event)
+    # path('show_event/',show_event),
+    # path('contact/',contact),
+    # path("show_event/<int:id>/",show_specific_event),
+    # path("show_event/<id>/",show_specific_event),
+    path("dashboard/",dashboard),
+    path("test/",test),
 ]
--- events/views.py
+++ events/views.py
@@ -2,16 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# def show_event(request):
+#     return HttpResponse("<h1 style='color:green'>This is our event list</h1>")
+
+# def contact(request):
+#     return HttpResponse("<h1>This is our contact page</h1>")
+
+# def show_specific_event(request, id):
+#     print("id", id)
+#     print("id type", type(id))
+#     return HttpResponse(f"This is specific event page {id}");
+
 def home(request):
     return HttpResponse("<h1 style='color:red'>Welcome To The Event Management System</h1>")
 
-def show_event(request):
-    return HttpResponse("<h1 style='color:green'>This is our event list</h1>")
+def dashboard(request):
+    return render(request, "dashboard.html")
 
-def contact(request):
-    return HttpResponse("<h1>This is our contact page</h1>")
-
-def show_specific_event(request, id):
-    print("id", id)
-    print("id type", type(id))
-    return HttpResponse(f"This is specific event page {id}");
+def test(request):
+    return render(request, "test.html")
